fa5de54cdc2d98b7c7430569bbe2a8b0ef05cd1f
Sahasra Ranjan <sahasraranjan@gmail.com>
2020-11-04 19:38:27 +0530

    bst done

---
+++ c/readwrite/Tree.java
@@ -0,0 +1,57 @@
+package readwrite;
+
+class Tree{
+    boolean assigned;
+    int key;
+    Tree left, right;
+
+    Tree(){
+        assigned = false;
+        left = right = null;
+    }
+
+    public void write(int _key){
+        if(assigned == false){
+            key = _key;
+            assigned = true;
+            return;
+        }
+
+        if(key < _key){
+            if(right == null){
+                right = new Tree();
+            }
+            right.write(_key);
+        }else{
+            if(left == null){
+                left = new Tree();
+            }
+            left.write(_key);
+        }
+        return;
+    }
+
+    public int read(int _key){
+        if(key == _key && assigned) return _key;
+        
+        if(_key < key){
+            if(left == null) return key;
+            return left.read(_key);
+        }else{
+            if(right == null) return key;
+            return right.read(_key);
+        }
+    }
+
+    // public static void main(String args[]) throws Exception{
+    //     Scanner sc = new Scanner(System.in);
+    //     Tree t = new Tree();
+    //     t.write(sc.nextInt());
+    //     t.write(sc.nextInt());
+    //     t.write(sc.nextInt());
+    //     t.write(sc.nextInt());
+
+    //     System.out.println(t.read(sc.nextInt()));
+    //     sc.close();
+    // }
+}
