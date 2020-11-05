608b910179f71dddb7629428f810e114c72d87e7
Sahasra Ranjan <sahasraranjan@gmail.com>
2020-11-05 08:51:46 +0530

    8c part1 Tree

---
+++ 8/resources/P4/Sieve.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Sieve{
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+
+
+        System.out.println(n);
+        sc.close();
+    }
+}
---
+++ c/readwrite/ProtectedTree.java
@@ -0,0 +1,14 @@
+package readwrite;
+
+class ProtectedTree{
+    ProtectedTree ptree;
+
+    ProtectedTree(Tree t){
+        
+    }
+
+
+    public static void main(String args[]){
+        ProtectedTree ptree = new ProtectedTree(new Tree());
+    }
+}
