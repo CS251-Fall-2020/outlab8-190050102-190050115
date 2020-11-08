fc61ad7c304576214b32d60d9a54dad4b9e3accc
Sahasra Ranjan <sahasraranjan@gmail.com>
2020-11-08 20:06:36 +0530

    Sieve done

--- 8/resources/P4/Sieve.java
+++ 8/resources/P4/Sieve.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Sieve{
 
@@ -7,8 +9,28 @@ class Sieve{
         int n;
         n = sc.nextInt();
 
+        Boolean [] prime = new Boolean[n+1];
+        Arrays.fill(prime, true);
 
-        System.out.println(n);
+        IntStream.range(2, n).boxed().collect(Collectors.toList()).stream()
+            .forEach(x->{
+                if(prime[x]==true){
+                    IntStream.range(x, n/x+1).boxed().collect(Collectors.toList()).stream()
+                        .forEach(y->{
+                            prime[x*y] = false;
+                        });
+                }
+            });
+
+        IntStream.range(2, n).boxed().collect(Collectors.toList()).stream()
+            .forEach(x->{
+                if(prime[x] == true){
+                    System.out.printf("%d ", x);
+                }
+            });
+        
+        System.out.println("");
+
         sc.close();
     }
 }
